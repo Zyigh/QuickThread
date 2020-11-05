ac1994d24f259914150c00dd55bc5f76324087c5
Zyigh <hugo.medina@hetic.net>
2020-11-05 23:06:04 +0100

    New: adds alert on save and changes preview cta color (#3)
    Co-authored-by: Zyigh <hmedina.paris75@gmail.com>

--- src/views/TwitterThreader.vue
+++ src/views/TwitterThreader.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="container-fluid">
     <div class="row">
+      <b-alert
+        class="mt-4 col-md-6 offset-md-3 position-fixed z-index"
+        :show="timeBeforeClosingAlert"
+        :dismissible="true"
+        :fade="true"
+        variant="success"
+        @dismiss-count-down="countDownChanged"
+      >
+        <p class="p-0 m-0">
+          Your content has been saved successfuly ! It will be available next time you come here.
+        </p>
+    </b-alert>
+    </div>
+    <div class="row">
       <div class="col-md-6">
-        <TweetEditor/>
+        <TweetEditor @contentSaved="showSuccessAlert" />
       </div>
       <div class="col-md-6">
         <TweetsPreview v-if="hasElements"/>
@@ -11,8 +25,15 @@
   </div>
 </template>
 
+<style scoped>
+  .z-index {
+    z-index: 8;
+  }
+</style>
+
 <script lang="ts">
 import Vue from 'vue';
+
 import TweetEditor from '../components/TweetEditor.vue';
 import TweetsPreview from '../components/TweetsPreview.vue';
 
@@ -22,12 +43,25 @@ export default Vue.extend({
     TweetEditor,
     TweetsPreview,
   },
+  data() {
+    return {
+      timeBeforeClosingAlert: 0 as number,
+    };
+  },
   computed: {
     tweetsFormatted(): string[] {
       return this.$store.getters.tweetsFormatted;
     },
     hasElements(): boolean {
       return 0 < this.tweetsFormatted.length;
+    },
+  },
+  methods: {
+    showSuccessAlert() {
+      this.timeBeforeClosingAlert = 5;
+    },
+    countDownChanged(dismissCountDown: number) {
+      this.timeBeforeClosingAlert = dismissCountDown;
     },
   },
 });
