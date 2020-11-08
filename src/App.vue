bcd4daad8559b50ec32318ded4917bed86b4c7c5
Zyigh <hugo.medina@hetic.net>
2020-11-08 16:18:52 +0100

    New: Adds logo and Header + modify title and favicon (#12)
    Co-authored-by: Zyigh <hmedina.paris75@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <Header/>
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import Header from './components/commons/Header.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Header,
+  },
+});
+</script>
---
+++ src/components/commons/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-1 col-md-2 col-3">
+        <h1>
+          <img
+            src="../../assets/logo-low.png"
+            alt="QuickThread - A simple tool to write threads on Twitter"
+            class="w-100"
+          />
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Header',
+});
+</script>
